0ea371711079c55eab7754d12e93abc45abbf906
Sam Johns <samkjohns@gmail.com>
2016-06-13 13:45:04 -0400

    Builds DBConnection class

---
+++ lib/db_connection.rb
@@ -0,0 +1,43 @@
+require 'sqlite3'
+
+PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'true'
+ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
+
+class DBConnection
+  def self.open(db_file_name)
+    @db = SQLite3::Database.new(db_file_name)
+    @db.results_as_hash = true
+    @db.type_translation = true
+
+    @db
+  end
+
+  def instance; @db; end
+
+  def self.execute(*args)
+    print_query(*args)
+    instance.execute(*args)
+  end
+
+  def self.execute2(*args)
+    print_query(*args)
+    instance.execute2(*args)
+  end
+
+  def self.last_insert_row_id
+    instance.last_insert_row_id
+  end
+
+  private
+
+  def self.print_query(query, *interpolation_args)
+    return unless PRINT_QUERIES
+
+    puts '--------------------'
+    puts query
+    unless interpolation_args.empty?
+      puts "interpolate: #{interpolation_args.inspect}"
+    end
+    puts '--------------------'
+  end
+end
